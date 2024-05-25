e16869e5cf442451d357f3650f17e2b62bf17850
barin2010 <uyrirudenko@gmail.com>
2024-05-26 00:14:09 +0300

    added perfomance

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import { Header } from './header/Header';
 import Private from './private/Private';
+import Perfomance from './perfomanse/Perfomance';
 
 export const App = () => {
   return (
     <div>
       <Header />
       <Private />
+      <Perfomance />
     </div>
   );
 };
---
+++ src/components/perfomanse/Perfomance.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import css from './Perfomance.module.css';
+
+const Perfomance = () => {
+  return <div>Perfomance</div>;
+};
+
+export default Perfomance;
